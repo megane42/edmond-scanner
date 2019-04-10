02f1c1b145eac9bf8430cffac5e03876638b0acc
megane42 <h.kzm.gm@gmail.com>
2019-04-10 13:34:33 +0900

    change endpoint to edmond-ql (with graphql)

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
-const readline = require('readline')
-const axios    = require('axios')
+const readline    = require('readline')
+const { request } = require('graphql-request')
 
 const rl = readline.createInterface({
   input:  process.stdin,
   output: process.stdout,
 })
 
-const api_url = process.env.EDMOND_API_URL
+const endpoint = process.env.EDMOND_API_URL
+// const endpoint = "http://localhost:8888/graphql"
 
 console.log("Waiting for ISBN input...")
 
 rl.on('line', (input) => {
-  axios.post(api_url + input)
-       .then((res) => {
-         console.log(
-           {
-             message: `The book (ISBN: ${input}) is successfully registered.`,
-             http_status: `${res.status}`,
-             body: res.data,
-           }
-         )
-       })
-       .catch((err) => {
-         console.log(
-           {
-             message: `The book (ISBN: ${input}) is not registerd due to some errors.`,
-             http_status: `${err.response.status}`,
-             body: err.response.data,
-           }
-         )
-       })
+  request(endpoint, `
+      mutation add_book_from_edmond_scanner {
+        register_book(isbn: ${input}) {
+          title
+          author
+          isbn
+          stock_count
+        }
+      }
+  `
+  ).then((data) => {
+    console.log(
+      {
+        message: `The book (ISBN: ${input}) is successfully registered.`,
+        data: data,
+      }
+    )
+  }).catch((err) => {
+    console.log(
+      {
+        message: `The book (ISBN: ${input}) is not registerd due to some errors.`,
+        http_status: err.response.status,
+        error: err.response.error,
+      }
+    )
+  })
 
   console.log(`Received: ${input}`)
 })
